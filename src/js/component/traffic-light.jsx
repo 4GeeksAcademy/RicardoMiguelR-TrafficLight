d583c78534bc66953c73e3f1dce27e8605fe98be
Ricardo Miguel R <ricardomr.23@hotmail.com>
2024-03-15 23:08:23 +0000

    My Commit2

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -2,45 +2,45 @@ import React, {useState} from "react";
 
 const TrafficLight = () => {
 
-    const [redBackground, setRedBackground] = useState('red');
-    const [orangeBackground, setOrangeBackground] = useState('orange');
-    const [greenBackground, setGreenBackground] = useState('green');
+    const [redBackground, setRedBackground] = useState({color: 'red', shadow: false});
+    const [orangeBackground, setOrangeBackground] = useState({color: 'orange', shadow: false});
+    const [greenBackground, setGreenBackground] = useState({color: 'green', shadow: false});
     function red() {
-        if (redBackground == 'red') {
-            setRedBackground('white')
-            setOrangeBackground('orange')
-            setGreenBackground('green')
-        }else {
-            setRedBackground('red')
+        if (redBackground.color == 'red') {
+            if (redBackground.shadow){
+                setRedBackground({color: 'red', shadow: false}) 
+            }else{setRedBackground({color: 'red', shadow: true})
+            setOrangeBackground({color: 'orange', shadow: false})
+            setGreenBackground({color: 'green', shadow: false})}
         }
     };
 
     function orange() {
-        if (orangeBackground == 'orange') {
-            setOrangeBackground('white')
-            setRedBackground('red')
-            setGreenBackground('green')
-        }else {
-            setOrangeBackground('orange')
+        if (orangeBackground.color == 'orange') {
+            if (orangeBackground.shadow){
+                setOrangeBackground({color: 'orange', shadow: false}) 
+            }else{setOrangeBackground({color: 'orange', shadow: true})
+            setRedBackground({color: 'red', shadow: false})
+            setGreenBackground({color: 'green', shadow: false})}  
         }
     };
 
     function green() {
-        if (greenBackground == 'green') {
-            setGreenBackground('white')
-            setOrangeBackground('orange')
-            setRedBackground('red')
-        }else {
-            setGreenBackground('green')
+        if (greenBackground.color == 'green') {
+            if (greenBackground.shadow){
+                setGreenBackground({color: 'green', shadow: false}) 
+            }else{setGreenBackground({color: 'green', shadow: true})
+            setOrangeBackground({color: 'orange', shadow: false})
+            setRedBackground({color: 'red', shadow: false})}   
         }
     };
 
     return (
         <>
             <div className="btn-group-vertical position-relative p-3 m-5" style={{background: 'black', borderRadius: '3cap'}} role="group" aria-label="Vertical button group">
-                <button style={{borderRadius: '50%', width: '145px', height: '145px', backgroundColor: redBackground}} onClick={red}></button>
-                <button style={{borderRadius: '50%', marginTop: '10px', width: '145px', height: '145px', background: orangeBackground}} onClick={orange}></button>
-                <button style={{borderRadius: '50%', width: '145px', marginTop: '10px', height: '145px', background: greenBackground}} onClick={green}></button>
+                <button style={{borderRadius: '50%', width: '145px', height: '145px', backgroundColor: redBackground.color, boxShadow: redBackground.shadow ? '-2px 5px 59px 18px rgba(186,186,186,1)' : null}} onClick={red}></button>
+                <button style={{borderRadius: '50%', marginTop: '10px', width: '145px', height: '145px', background: orangeBackground.color, boxShadow: orangeBackground.shadow ? '-2px 5px 59px 18px rgba(186,186,186,1)' : null}} onClick={orange}></button>
+                <button style={{borderRadius: '50%', width: '145px', marginTop: '10px', height: '145px', background: greenBackground.color, boxShadow: greenBackground.shadow ? '-2px 5px 59px 18px rgba(186,186,186,1)' : null}} onClick={green}></button>
             </div>
             <div className="position-sticky bottom-50 end-100" style={{background: 'black', height: '1px', width: '595px', height: '60px'}}></div>
         </>
